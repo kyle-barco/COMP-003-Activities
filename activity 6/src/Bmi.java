90f21356620aad4d26effa6a0bd9eccd985e4b75
kyle-barco <kylebarco04@gmail.com>
2025-11-14 06:25:37 +0800

    initial commit

---
+++ activity 6/src/Bmi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Bmi {
+    public static void main(String[] args) {
+        System.out.print("Enter Your weight in pounds");
+        Scanner scan = new Scanner(System.in);
+        double weightInPounds = scan.nextDouble();
+        System.out.print("Enter your height in feet followed\n");
+
+        System.out.print("By a space then additional inches");
+        double height = scan.nextDouble();
+
+
+        // bmi calculation
+            // convert pound to kilogram
+            // height in feet
+
+
+        // bmi value
+
+
+
+    }
+}
