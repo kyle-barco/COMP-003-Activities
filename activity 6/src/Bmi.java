2a7ceb8c9e3b034409749816ea8cec4d4727bef2
kyle-barco <kylebarco04@gmail.com>
2025-11-14 18:53:12 +0800

    update

--- activity 6/src/Bmi.java
+++ activity 6/src/Bmi.java
@@ -2,23 +2,33 @@ import java.util.Scanner;
 
 public class Bmi {
     public static void main(String[] args) {
-        System.out.print("Enter Your weight in pounds");
+        double CONVERTVAL = 0.45359237, FTinM = 0.3048, INCHinM = 0.0254, mass, height, bmi;
+        String categoryArr[] =  {"Underweight", "Normal weight", "Overweight", "Obese"};
+
+
+        System.out.print("Enter Your weight in pounds: ");
         Scanner scan = new Scanner(System.in);
         double weightInPounds = scan.nextDouble();
         System.out.print("Enter your height in feet followed\n");
 
-        System.out.print("By a space then additional inches");
-        double height = scan.nextDouble();
+        System.out.print("By a space then additional inches: ");
+        double heightInFeet = scan.nextDouble();
+        double heightInInches = scan.nextDouble();
 
 
-        // bmi calculation
-            // convert pound to kilogram
-            // height in feet
+        // *** bmi calculation ***
+        // convert pound to kilogram
+        double kgWeight = weightInPounds * CONVERTVAL;
 
+        // height in feet (
+        double mHeight = heightInFeet * FTinM + heightInInches * INCHinM;
 
         // bmi value
+        bmi = kgWeight / Math.pow(mHeight, 2);
 
+        // *** choosing category ***
+        for()
 
-
+            System.out.println("Your bmi is: " + bmi);
     }
 }
