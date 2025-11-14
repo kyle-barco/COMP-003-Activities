7065606d7bf3bb94349fb2c75ce70d055d4cc51f
kyle-barco <kylebarco04@gmail.com>
2025-11-14 19:28:58 +0800

    update

--- activity 6/src/Bmi.java
+++ activity 6/src/Bmi.java
@@ -2,23 +2,36 @@ import java.util.Scanner;
 
 public class Bmi {
     public static void main(String[] args) {
-        System.out.print("Enter Your weight in pounds");
+        double CONVERTVAL = 0.45359237, FTinM = 0.3048, INCHinM = 0.0254, bmi;
+
+        System.out.print("Enter Your weight in pounds: ");
         Scanner scan = new Scanner(System.in);
         double weightInPounds = scan.nextDouble();
         System.out.print("Enter your height in feet followed\n");
 
-        System.out.print("By a space then additional inches");
-        double height = scan.nextDouble();
+        System.out.print("By a space then additional inches: ");
+        double heightInFeet = scan.nextDouble();
+        double heightInInches = scan.nextDouble();
 
+        // convert pound to kilogram
+        double kgWeight = weightInPounds * CONVERTVAL;
+
+        // height in feet (
+        double mHeight = heightInFeet * FTinM + heightInInches * INCHinM;
 
         // bmi calculation
-            // convert pound to kilogram
-            // height in feet
+        bmi = kgWeight / Math.pow(mHeight, 2);
+        double roundedBmi = Math.ceil(bmi * 100.0) / 100.0;
 
+        // category
+        String category = (bmi < 18.5)
+                        ? "Underweight" : (bmi < 25)
+                        ? "Normal weight" : (bmi < 30)
+                        ? "Overweight" : (bmi >= 30)
+                        ? "Obese": "Enter a valid input" ;
+        System.out.println("Your BMI is: " + roundedBmi);
+        System.out.println("Your risk category is : " + category);
 
-        // bmi value
-
-
-
+        System.out.println("\nPROGRAMMED BY: : KYLE BARCO");
     }
 }
