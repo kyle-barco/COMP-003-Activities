282edcf115a4292a8185cd16ba671fd3080542b1
kyle-barco <kylebarco04@gmail.com>
2025-11-17 05:45:54 +0800

    update for submission

--- activity 7/src/Bmr.java
+++ activity 7/src/Bmr.java
@@ -1,22 +1,5 @@
 import java.util.Scanner;
 
-
-//The calories needed for a Female to maintain her weight is:
-//BMR = 655 + (4.3 x weight in pounds) + (4.7 x height in inches) – (4.7 x age in years)
-//The calories needed for a Male to maintain his weight is:
-//BMR = 660 + (6.3 x weight in pounds) + (12.9 x height in inches) – (6.8 x age in years)
-
-//A typical chocolate bar will contain around 230 calories. Write a program that allows the user to input the
-//gender, their weight in pounds, height in inches and age in years.
-//        In addition, ask the user about her/his lifestyle, if he or she is:
-//          1. Sedentary (somewhat inactive)
-//          2. Somewhat active (exercise occasionally)
-//          3. Active (exercise 3-4 days a week)
-//          4. Highly active (exercise everyday)
-//If the user is “Sedentary” then increase the calculated BMR by 20%. If the user is “Somewhat active” then
-//increase the calculated BMR to 30%. If the user is “Active” then increase the BMR by 40% and increase by 50%
-//        if the user is “Highly active”. Then output the new BMR value.
-
 public class Bmr {
     public static void main(String[] args) {
         String[] strLifestyle = {"SEDENTARY (SOMEWHAT INACTIVE)",
@@ -24,44 +7,66 @@ public class Bmr {
                                 "ACTIVE (EXERCISE 3-4 DAYS A WEEK)",
                                 "HIGHLY ACTIVE (EXERCISE EVERYDAY)"};
         double FEMALE_FIX_WEIGHT = 4.3, FEMALE_FIX_HEIGHT = 4.7, FEMALE_FIXED_AGE = 4.7,
-               MALE_FIX_WEIGHT = 6.3, MALE_FIX_HEIGHT = 12.9, MALE_FIXED_AGE = 6.8, bmr = 0, newBmr;
+               MALE_FIX_WEIGHT = 6.3, MALE_FIX_HEIGHT = 12.9, MALE_FIXED_AGE = 6.8, bmr = 0, addBmr, newBmr;
         String lifestyle = "";
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("ENTER YOUR NAME: ");
+        System.out.print("ENTER YOUR NAME               : ");
         String nameInput = scan.next();
 
-        System.out.println("ENTER YOUR AGE: ");
+        System.out.print("ENTER YOUR AGE                : ");
         int ageInput = scan.nextInt();
 
-        System.out.println("ENTER YOUR WEIGHT (in pounds)");
+        System.out.print("ENTER YOUR WEIGHT (in pounds) : ");
         double weightInput = scan.nextDouble();
 
-        System.out.println("ENTER YOUR HEIGHT (in inches)");
+        System.out.print("ENTER YOUR HEIGHT (in inches) : ");
         int heightInput = scan.nextInt();
 
-        System.out.println("ENTER YOUR GENDER");
+        System.out.print("ENTER YOUR GENDER             : ");
         String genderInput = scan.next();
 
         // bmr calculation
-        if(genderInput == "female" || genderInput == "Female") {
-            bmr = 655 + (FEMALE_FIX_WEIGHT * weightInput) + (FEMALE_FIX_HEIGHT * heightInput) - (FEMALE_FIXED_AGE - ageInput);
-        } else if (genderInput == "male"  || genderInput == "Female") {
-            bmr = 660 + (MALE_FIX_WEIGHT * weightInput) + (MALE_FIX_HEIGHT * heightInput) - (MALE_FIXED_AGE - ageInput);
+        if(genderInput.equalsIgnoreCase("female")) {
+            bmr = 655 + (FEMALE_FIX_WEIGHT * weightInput) + (FEMALE_FIX_HEIGHT * heightInput) - (FEMALE_FIXED_AGE * ageInput);
+        } else if (genderInput.equalsIgnoreCase("male")) {
+            bmr = 660 + (MALE_FIX_WEIGHT * weightInput) + (MALE_FIX_HEIGHT * heightInput) - (MALE_FIXED_AGE * ageInput);
         }
-        System.out.println("YOUR BMR IS: " + bmr);
+        System.out.println("YOUR BMR IS                   : " + bmr + " Calories/day\n");
 
         // lifestyle
+        System.out.print("ENTER YOUR LIFESTYLE: \n");
         for(int i = 1; i <= strLifestyle.length; i++) {
-            System.out.println(i+ " " + strLifestyle[i]);
+            System.out.println("\t" + i + " " + strLifestyle[i-1]);
         }
-        System.out.println("ENTER YOUR LIFESTYLE: ");
+        System.out.print("ENTER YOUR LIFESTYLE          : ");
+        scan.nextLine();
+        lifestyle = scan.nextLine();
+
         switch (lifestyle) {
             case "SEDENTARY", "Sedentary":
                 newBmr = bmr * 0.2;
                 break;
+            case "Somewhat active", "somewhat active", "SOMEWHAT ACTIVE":
+                addBmr = bmr * 0.3;
+                newBmr = bmr + addBmr;
+                break;
+            case "Active", "active", "ACTIVE":
+                addBmr = bmr * 0.4;
+                newBmr = bmr + addBmr;
+                break;
+            case "Highly active", "highly active", "HIGHLY ACTIVE":
+                addBmr =  bmr * 0.5;
+                newBmr = bmr + addBmr;
+                break;
+            default:
+                addBmr = 0;
+                newBmr = bmr;
+                System.out.print("Enter valid input\n");
         }
 
+        System.out.print("YOUR NEW BMR IS               : " + newBmr + " Calories/day\n");
+        System.out.print("PROGRAMMED BY                 : KYLE BARCO");
     }
 }
